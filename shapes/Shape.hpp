ce87b5bec51c9cc656e2ec667ab79e9941855ce1
Andrzej Rafalski <anraf1001@gmail.com>
2020-09-28 19:36:36 +0200

    Color

--- shapes/Shape.hpp
+++ shapes/Shape.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
-class Shape
-{
+enum class Color : unsigned char {
+    Red = 'r',
+    Green = 'g',
+    Blue = 'b'
+};
+
+class Shape {
 public:
     virtual ~Shape() {}
 
     virtual double getArea() const = 0;
     virtual double getPerimeter() const = 0;
     virtual void print() const;
+    virtual Color getColor() const { return color_; }
+
+private:
+    Color color_ = Color::Red;
 };
